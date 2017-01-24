edecf4f8e6ca7f192a639425dfbb5f7506ec2f00
ilya.maksimenko <ilya.maksimenko@ESW350>
2017-01-24 13:26:08 +0300

    End of stage 2

--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -10,6 +10,7 @@ namespace ContosoUniversity.DAL
         public SchoolContext() : base("SchoolContext")
         {
         }
+        
 
         public DbSet<Student> Students { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
